3d63c6b84945cbc91c5a361b696c31fde02e2080
Ayodeji Abodunrin <Ayodeji.abodunrin@wemabank.com>
2021-12-04 05:11:45 +0100

    removed server folder

--- server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const authServices = require("../services/auth");
-
-router.post("/", (req, res) => authServices.login(req, res));
-
-module.exports = router;
